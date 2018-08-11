0500cf68a14d17301860dbfa29a84a4e809b33f5
sandeeptile <sandeeptile@gmail.com>
2018-08-11 21:54:25 +0530

    category_indiview added

--- app/src/main/java/com/example/sandy/simple_shopping_app/Controller/MainActivity.kt
+++ app/src/main/java/com/example/sandy/simple_shopping_app/Controller/MainActivity.kt
@@ -2,14 +2,25 @@ package com.example.sandy.simple_shopping_app.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.sandy.simple_shopping_app.Model.Category
 import com.example.sandy.simple_shopping_app.R
+import com.example.sandy.simple_shopping_app.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var adapter:ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        adapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,DataService.categories)
+
+
+        categoryListView.adapter=adapter
 
     }
 }
--- app/src/main/java/com/example/sandy/simple_shopping_app/Model/Category.kt
+++ app/src/main/java/com/example/sandy/simple_shopping_app/Model/Category.kt
@@ -1,3 +1,8 @@
 package com.example.sandy.simple_shopping_app.Model
 
-class Category (val title:String,val image:String)
+class Category (val title:String,val image:String){
+
+    override fun toString(): String {
+        return title
+    }
+}
