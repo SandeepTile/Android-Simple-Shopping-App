e576aa4a2244eda5958984c23d587ca089524da1
sandeeptile <sandeeptile@gmail.com>
2018-08-11 23:09:43 +0530

    category view done

--- app/src/main/java/com/example/sandy/simple_shopping_app/Controller/MainActivity.kt
+++ app/src/main/java/com/example/sandy/simple_shopping_app/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sandy.simple_shopping_app.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.sandy.simple_shopping_app.Adapters.CategoryAdapter
 import com.example.sandy.simple_shopping_app.Model.Category
 import com.example.sandy.simple_shopping_app.R
 import com.example.sandy.simple_shopping_app.Services.DataService
@@ -10,14 +11,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-   lateinit var adapter:ArrayAdapter<Category>
+   lateinit var adapter:CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        adapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,DataService.categories)
+        adapter= CategoryAdapter(this,DataService.categories)
 
 
         categoryListView.adapter=adapter
